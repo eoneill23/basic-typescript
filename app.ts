34df5b8368b776b2c2ecc9e48edda89e15207d4f
Eric O'Neill <oneill.eric23@gmail.com>
2021-01-30 18:21:58 -0700

    Unknown type

--- app.ts
+++ app.ts
@@ -1,17 +1,8 @@
-function add (n1: number, n2: number) {
-  return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'Eric';
+if(typeof userInput === 'string') {
+  userName = userInput;
 }
-
-function printResult(num: number): void {
-  console.log('Result: ' + num);
-}
-
-printResult((add(5, 12)));
-
-let combineValues: (a: number, b: number) => number;
-
-combineValues = add;
-combineValues = printResult;
-//combineValues = 5;
-
-console.log(combineValues(8, 8));
